51aba6b0f040d427ebd3390e8d3c5424ae5ceb62
adda-sil <adrien@fernandes.bzh>
2021-12-13 01:39:49 +0100

    start d03

---
+++ d03/ex00/ClapTrap.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adda-sil <adda-sil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/12 00:17:40 by adda-sil          #+#    #+#             */
+/*   Updated: 2021/12/12 23:36:50 by adda-sil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ClapTrap_HPP
+# define ClapTrap_HPP
+
+# include <iostream>
+# include <string>
+
+class ClapTrap;
+
+class ClapTrap {
+    private:
+        std::string             _name;
+        int                     _hit_points;
+        int                     _energy_points;
+    public:
+        ClapTrap               ();
+        ClapTrap               (const ClapTrap &f);
+        ~ClapTrap              ();
+};
+
+#endif
---
+++ d03/ex00/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adda-sil <adda-sil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/12 00:17:45 by adda-sil          #+#    #+#             */
+/*   Updated: 2021/12/12 00:25:57 by adda-sil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int
+    main(void)
+{
+    Fixed a;
+    Fixed b(a);
+    Fixed c;
+
+    c = b;
+
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+
+    return (0);
+}
